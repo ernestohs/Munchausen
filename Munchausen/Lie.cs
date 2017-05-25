24b67bc2aa6a0663adac3d375870582d7e78a28d
ernestohs <ernestohs@gmail.com>
2017-05-25 14:07:20 -0500

    Refactor Geenrator

--- Munchausen/Lie.cs
+++ Munchausen/Lie.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection;
-using System.Threading.Tasks;
 
 namespace Munchausen
 {
+
     [SuppressMessage("ReSharper", "StaticMemberInGenericType")]
     public sealed class Lie<T>
     {
@@ -17,17 +16,10 @@ namespace Munchausen
 
         private static readonly Func<Expression, object> ArgumentsFromExpression =
             argument => Expression.Lambda(argument).Compile().DynamicInvoke();
-
-        private static readonly Dictionary<Type, Func<int, object>> TypeHandlers = new Dictionary<Type, Func<int, object>>
-        {
-            {typeof(int), i => i }
-        };
-
+        
         private static object[] ConstructorArguments { get; set; }
 
-        private Lie()
-        {
-        }
+        private Lie() { }
 
         public Lie<T> WithConstructor(Expression<Func<T>> constructor)
         {
@@ -38,9 +30,34 @@ namespace Munchausen
 
             var arguments = ((NewExpression) constructor.Body).Arguments;
 
-            ConstructorArguments = arguments.Select(ArgumentsFromExpression).ToArray<object>();
+            ConstructorArguments = arguments.Select(ArgumentsFromExpression).ToArray();
 
             return Instance;
+        }
+
+        private static T CreateInstance(long index = 0)
+        {
+            var instance = ConstructInstance();
+
+            var transformator = new ModelGenerator<T>(instance, index);
+
+            return transformator.GetInstance();
+        }
+
+        private static T ConstructInstance()
+        {
+            T instance;
+
+            if (ConstructorArguments == null)
+            {
+                instance = Activator.CreateInstance<T>();
+            }
+            else
+            {
+                instance = (T) Activator.CreateInstance(typeof(T), ConstructorArguments);
+            }
+
+            return instance;
         }
 
         public static T Generate()
@@ -48,42 +65,7 @@ namespace Munchausen
             return CreateInstance();
         }
 
-        // public 
-
-        private static T CreateInstance(int index = 0)
-        {
-            T instance;
-            if (ConstructorArguments == null)
-            {
-                instance = Activator.CreateInstance<T>();
-            }
-            else
-            {
-                instance = (T)Activator.CreateInstance(typeof(T), ConstructorArguments);
-            }
-
-            Parallel.ForEach(typeof(T).GetProperties(), DynamicSetValue(instance, index));
-
-            return instance;
-        }
-
-        private static Action<PropertyInfo> DynamicSetValue(T instance, int index)
-        {
-            return fieldInfo => fieldInfo.SetValue(instance, GenerateValue(fieldInfo.PropertyType, index));
-        }
-
-        [SuppressMessage("ReSharper", "ConvertIfStatementToReturnStatement")]
-        private static object GenerateValue(Type type, int index)
-        {
-            if (TypeHandlers.ContainsKey(type))
-            {
-                return TypeHandlers[type](index);
-            }
-
-            return default(Type);
-        }
-
-        public static IList<T> Generate(int number)
+        public static IList<T> Generate(long number)
         {
             var results = new List<T>();
 
---
+++ Munchausen/ModelGenerator.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Munchausen
+{
+    public class ModelGenerator<T>
+    {
+        private T _target;
+        private readonly long _index;
+        private readonly List<Func<T, T>> _functions = new List<Func<T, T>>();
+        private bool _executed = false;
+
+        private static readonly Dictionary<Type, Func<long, object>> TypeHandlers = new Dictionary<Type, Func<long, object>>
+        {
+            {typeof(int), i => (int)i }
+        };
+
+        public ModelGenerator(T target, long index)
+        {
+            _target = target;
+            _index = index;
+        }
+
+        public ModelGenerator(T target, List<Func<T, T>> funcs)
+        {
+            _target = target;
+            _functions = funcs;
+        }
+
+        public ModelGenerator<T> AddFunction(Func<T, T> function)
+        {
+            _functions.Add(function);
+
+            return this;
+        }
+
+        public ModelGenerator<T> AddFunctions(List<Func<T, T>> function)
+        {
+            _functions.AddRange(function);
+
+            return this;
+        }
+
+        public ModelGenerator<T> ApplyFunctions()
+        {
+            foreach (var function in _functions)
+            {
+                _target = function(_target);
+            }
+
+            _executed = true;
+            return this;
+        }
+
+        public T GetInstance()
+        {
+            if (_executed) return _target;
+
+            if (_functions.Count > 0)
+            {
+                this.ApplyFunctions();
+            }
+            else
+            {
+                AutoFillFields();
+                _executed = true;
+            }
+
+            return _target;
+        }
+
+        private void AutoFillFields()
+        {
+            Parallel.ForEach(typeof(T).GetProperties(), DynamicSetValue);
+        }
+
+        private void DynamicSetValue(PropertyInfo propertyInfo, ParallelLoopState state)
+        {
+            propertyInfo.SetValue(_target, GenerateSequencialValue(propertyInfo, _index));
+        }
+
+        private object GenerateSequencialValue(PropertyInfo propertyInfo, long index)
+        {
+            Type type = propertyInfo.PropertyType;
+            var info = type.GetTypeInfo();
+
+            if (info.IsValueType)
+            {
+
+                if (TypeHandlers.ContainsKey(type))
+                {
+                    return TypeHandlers[type](index);
+                }
+
+                return Activator.CreateInstance(type);
+            }
+
+            return null;
+        }
+
+    }
+}
